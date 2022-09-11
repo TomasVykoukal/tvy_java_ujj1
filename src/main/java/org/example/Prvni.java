ffe81877c0f3006ef56534af84f570274c3a6b6e
TomasVykoukal <tomas@vykoukal.com>
2022-09-11 11:49:05 +0200

    První prográmek z UJJ 2001/S. 25

---
+++ src/main/java/org/example/Prvni.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Prvni {
+    public static void main(String[] args) {
+        System.out.println("Ahoj sv\u011Bte!");
+        System.out.println("Hallo Rei\u00DFn\u00E4gel!");
+    }
+}
